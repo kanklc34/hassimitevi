45cda958d61240df0387c2e6c70820d315243334
KAAN <kanklcx1903@gmail.com>
2026-07-25 10:08:30 +0300

    Deploy scriptleri ve base yolu düzeltmesi

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // GitHub Pages'e deploy ederken base'i repo adına göre ayarlamayı unutma.
-// Örn: repo adı "has-simit-evi" ise -> base: '/has-simit-evi/'
-// Kendi .com alan adına bağlarsan base: '/' yeterli.
+// Örn: repo adı "has-simit-evi" ise -> base: '/hassimitevi/'
+// Kendi .com alan adına bağlarsan base: '/hassimitevi/' yeterli.
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/hassimitevi/',
 })
